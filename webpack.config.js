0f26c9570f745e6743152de92c010e408f7e1bb7
Alex <g28xyz@ya.ru>
2021-02-01 23:19:43 +0700

    v4

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: "dist/assets",
+		filename: "bundle.js",
+		sourceMapFilename: "bundle.map",
+	},
+	devtool: "#source-map",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: ["babel-loader"],
+				query: {
+					presets: ["env", "stage-0", "react"],
+				},
+			},
+		],
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true,
+			warnings: false,
+			mangle: true,
+		}),
+	],
+};
